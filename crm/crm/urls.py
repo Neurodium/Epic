d881498b5f934c0b5b7ba87fba10942c0be58bb3
JCT <jvandlife@gmail.com>
2022-03-20 16:58:38 +0100

    Update urls.py

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -31,8 +31,6 @@ router.register('comingevent', ComingEventViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', LoginUser.as_view()),
     path('get/contracts/<client_id>/', contract_list),
     path('get/sales/<client_id>/', sales_contact_list),
